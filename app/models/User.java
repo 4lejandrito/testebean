1fc9e65b40f8428e3fd44e9290a7b43bc62af3db
MilkyWay[Kyle] <wds-kwl@globallogic.com>
2014-11-19 16:07:18 +0200

    Initial commit

---
+++ app/models/User.java
@@ -0,0 +1,20 @@
+package models;
+
+
+import play.db.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class User extends Model {
+
+    @Column
+    public String name;
+
+    @Column
+    public String test;
+
+    public static Finder<Long, User> find = new Finder<>(Long.class, User.class);
+
+}
---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "testModule"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
